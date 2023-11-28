a53371d37c2e5ed722d7755788ffe8042ebc6bbb
marwenrebah <6863@holbertonstudents.com>
2023-11-28 01:15:42 -1100

    push, pall

---
+++ push.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * push - pushes an  element to the stack;
+ * @stack: top of the stack
+ * @line_number:integer
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+stack_t *new_node;
+(void)line_number;
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+new_node->n = atoi(value);
+new_node->prev = NULL;
+new_node->next = *stack;
+if (*stack)
+{
+(*stack)->prev = new_node;
+}
+*stack = new_node;
+}
